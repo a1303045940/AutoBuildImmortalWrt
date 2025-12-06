e9225082f77bae36857cba867d722cb61ebc0387
a1303045940 <125786842+a1303045940@users.noreply.github.com>
2025-12-06 22:40:16 +0800

    Update custom-packages.sh

--- shell/custom-packages.sh
+++ shell/custom-packages.sh
@@ -41,7 +41,7 @@ CUSTOM_PACKAGES="$CUSTOM_PACKAGES tailscale"
 # 进阶设置 by sirpdboy 
 # 当luci-app-advancedplus插件开启时 需排除冲突项 luci-app-argon-config和luci-i18n-argon-config-zh-cn 减号代表排除
 
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-argon-config luci-i18n-argon-config-zh-cn"
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-argon-config luci-i18n-argon-config-zh-cn -luci-app-filemanager"
 
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-advancedplus l18n-advancedplus-zh-cn -luci-app-argon-config -luci-i18n-argon-config-zh-cn"
 # 网络测速 by sirpdboy 
